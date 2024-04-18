fa060e6b1432afcd45e858de607a835cc81aa28b
Giamp999 <83239722+Giamp999@users.noreply.github.com>
2024-04-18 13:38:28 +0200

    C code Speed Controller Update 18/04

--- myrio/c/src/pid_speed_controller.c
+++ myrio/c/src/pid_speed_controller.c
@@ -1,10 +1,13 @@
+#include <stdint.h>
 #include <stdio.h>
 
 // Function to implement the PI controller (it probably won't be used here)
 void pi_function(float reference_speed, float measured_speed, float Kp, float Ki, float Tasking_period,
-                 float *control_signal)
+                 float *control_signal, float Kf, float threshold, float steady_curr, int32_t no_Kf)
 {
     // Define static variables to store the integral term and previous error
+    static int counter = 0;
+    static int flag = 0;
     static float integral_sum = 0.0;
     static float error = 0.0;
 
@@ -23,8 +26,36 @@ void pi_function(float reference_speed, float measured_speed, float Kp, float Ki
         integral_sum = 0.0;
     }
 
-    // Calculate the control signal (output) using the PI controller formula
-    *control_signal = Kp * error + (Ki * integral_sum) * Tasking_period_s;
+    if (no_Kf == 0)
+    {
+        if (reference_speed <= 0 || (measured_speed > threshold * reference_speed))
+        {
+            flag = 0;
+        }
+        else
+        {
+            flag = 1;
+        }
+        if ((measured_speed > threshold * reference_speed) && (counter >= 0 && counter <= 2))
+        {
+            integral_sum = (steady_curr - Kp * error) / (Ki * Tasking_period_s);
+            counter++;
+        }
+        // Calculate the control signal (output) using the PI controller formula
+        *control_signal = Kp * error + (Ki * integral_sum) * Tasking_period_s + Kf * flag;
+    }
+    else
+    {
+        if (reference_speed <= 0)
+        {
+            flag = 0;
+        }
+        else
+        {
+            flag = 1;
+        }
+        *control_signal = Kp * error + (Ki * integral_sum) * Tasking_period_s + Kf * flag;
+    }
 }
 
 // Function to implement the PD controller (incorrect and not used)
@@ -60,15 +91,21 @@ void pd_function(float reference_speed, float measured_speed, float Kp, float Kd
  * @param[in] Kp
  * @param[in] measured_speed (estimated speed of the wheel) [rad/s]
  * @param[in] reference_speed (provided on the Labview front panel) [rad/s]
+ * @param[in] Kf
+ * @param[in] threshold
+ * @param[in] steady_curr
+ * @param[in] no_Kf
  *
  * @return control_signal (current) [A]
  * @author Geraldine Doussain
  */
-extern float PIcontroller(float Ki, float Kp, float measured_speed, float reference_speed, float Tasking_period)
+extern float PIcontroller(float Ki, float Kp, float measured_speed, float reference_speed, float Tasking_period,
+                          float Kf, float threshold, float steady_curr, int32_t no_Kf)
 {
     float control_signal; // Local variable to store the control signal
     // Call the PD controller function to get the control signal
-    pi_function(reference_speed, measured_speed, Kp, Ki, Tasking_period, &control_signal);
+    pi_function(reference_speed, measured_speed, Kp, Ki, Tasking_period, &control_signal, Kf, threshold, steady_curr,
+                no_Kf);
     if (control_signal > 35.0)
     {
         control_signal = 35.0;
